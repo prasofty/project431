e9ca8a0807c22c29daad061bfb976daf64ffef31
prasanna.boga <prasofty@gmail.com>
2015-10-21 18:01:15 +0530

    added angular bootstrap basic js files

--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -1,1 +1,20 @@
+'use strict';
+
+/* Initialize Parse */
+Parse.initialize("gluKproWCh1eTnFbhiFtjzNhuOzOZMAZBMXlk7Ci", "VnMpNi4Ei58fuFTkjdxk5jyCpuBpIbLNEaHuUVh8");
+
+angular
+    .module('project431', ['ui.router'])
+    .config(function($stateProvider, $urlRouterProvider){
+        $urlRouterProvider.when('/dashboard', '/dashboard/overview');
+        $urlRouterProvider.otherwise('/');
+
+        $stateProvider
+            .state('intro', {
+                url: '',
+                templateUrl: 'views/intro.html'
+            });
+    });
+
+
 
